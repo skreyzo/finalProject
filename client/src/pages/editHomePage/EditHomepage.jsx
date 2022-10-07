f04eb35ebd866777bb782ca2e85b198ae187773e
Ruslan Gamzaev <rrrruuus@yandex.ru>
2022-10-07 13:25:15 +0300

    'fix1'

--- client/src/pages/editHomePage/EditHomepage.jsx
+++ client/src/pages/editHomePage/EditHomepage.jsx
@@ -1,12 +1,34 @@
 import React, { useState } from 'react'
-import styles from "./edithomepage.module.css";
-import { useDispatch } from "react-redux";
+import Box from '@mui/material/Box';
+import TextareaAutosize from '@mui/base/TextareaAutosize';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+
 
 const EditHomepage = () => {
 
-const dispatch = useDispatch();
+  const [textHomePage, setTextHomePage] = useState({});
+  const [editHomePageText, setEditHomePageText] = useState(false);
+  const [editField, setEditField] = useState('');
+  const [file, setFile] = useState(null) 
 
-  const [textHomePage, setTextHomePage] = useState({ textHP: '' });
+  React.useEffect(() => {
+    (async () => {
+      const res = await fetch('http://localhost:3010/about', {
+        method: "GET",
+        credentials: "include",
+      });
+      const data = (await res.json()) || [];
+      console.log('data fetch homepage GET', data);
+      setTextHomePage(data);
+      setEditField(data.toptext);
+    })();
+  }, []);
+
+  const handleEditHomePageText = () => {
+    setEditHomePageText(true);    
+  }
 
   const onChangeHandler = (event) => {
     //console.log(event.target.value)
@@ -15,13 +37,9 @@ const dispatch = useDispatch();
 
   const onSubmitHandler = async (event) => {
     event.preventDefault();
-
-
-     try {
-      
+     try {      
       const response = await fetch("http://localhost:3010/admin/edithomepage", {
-        method: "PUT",
-        credentials: "include",
+        method: "PUT",        
         headers: {
           "Content-Type": "application/json",          
         },
@@ -31,13 +49,10 @@ const dispatch = useDispatch();
         throw new Error(
           `Error when adding: ${response.statusText} ${response.status}`
         );
-      const data = await response.json();
-      console.log(data)  
-      if (data.err) throw new Error(data.err);
-
-      //dispatch(addTodo(data.todo)); // запись с redux
+      const data = await response.json();  
+      console.log('data fetch homepage PUT', data)     
+      if (data.err) throw new Error(data.err);      
       setTextHomePage({value: ''})   // очищаем поле ввода
-
     } catch (err) {
       console.log(err);
       alert(err.message);
@@ -45,7 +60,94 @@ const dispatch = useDispatch();
   };
 
   return (    
-    <div>
+    <>
+      <Typography sx={{
+        textAlign: 'center',
+        fontSize: '25px',
+        mb: '15px',
+      }}>Edit Home Page</Typography>
+
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '50px',
+        mx: 'auto',
+        alignItems: 'center'
+      }}>
+        <Typography sx={{
+          textAlign: 'center',
+          fontSize: '25px',
+        }}>Edit something</Typography>
+        <Box sx={{
+          display: 'flex',
+          gap: '10px',
+          alignItems: 'end',
+          width: '60%',
+          mx: 'auto'
+        }}>
+            <TextareaAutosize
+            aria-label="minimum height"
+            minRows={4}
+            placeholder="Your Text here..."
+            style={{ width: 350 }}
+            /> 
+
+          {!handleEditHomePageText ? (
+            <>
+              <Typography variant="h5" align="left" color="text.secondary" paragraph>
+                {textHomePage.greeting}
+              </Typography>
+            </>
+          ) :
+            <>
+              <TextareaAutosize
+                aria-label="minimum height"
+                minRows={4}
+                placeholder="Your Text here..."
+                style={{ width: 350 }}
+                defaultValue={textHomePage.greeting}
+                onChange={(e) => setEditField(e.target.value)}
+              />
+              <Button variant="contained" onClick={onSubmitHandler}>Save</Button>
+            </>
+          }
+
+          
+          <Button variant="contained" onClick={handleEditHomePageText}>Edit</Button>
+        </Box>
+        <Box sx={{
+          display: 'flex',
+          gap: '37px',
+          width: '60%',
+          mx: 'auto'
+        }}>
+{/*           <form
+            encType="multipart/form-data"
+            onSubmit={submitPost}
+          >
+            <Button variant="contained" component="label" type="submit">
+              Upload Big Photo
+              <input name="bigPhoto" hidden 
+                     accept="image/*" 
+                     multiple type="file" 
+                     onChange={(e) => setFile(e.target.files[0])}
+                     id="bigPhoto"
+                    />
+            </Button>
+            <Button  variant="contained" component="label" type="submit">
+              Upload Team Photo
+              <input name="teamPhoto" 
+                     hidden accept="image/*" 
+                     multiple type="file" 
+                     onChange={(e) => setFile(e.target.files[0])}
+                     id="file"
+                     />
+            </Button>
+          </form> */}
+        </Box>
+      </Box>
+    </>
+/*     <div>
       <div>EditHomepage</div>
       <form
       onSubmit={onSubmitHandler}
@@ -59,7 +161,7 @@ const dispatch = useDispatch();
         <button type="submit">Save</button>
        
       </form>      
-    </div>
+    </div> */
   )
 }
 
