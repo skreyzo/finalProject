87c41f3683c6f567d6be6eb6d22b3488f04930b1
Ruslan Gamzaev <rrrruuus@yandex.ru>
2022-10-14 02:27:26 +0300

    'end1'

--- server/db/models/event.js
+++ server/db/models/event.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.hasMany(models.Order, { foreignKey: 'EventId' });
+      this.belongsToMany(models.User, {through: models.Order});
       // define association here
     }
   }
--- server/src/controllers/eventControllers.js
+++ server/src/controllers/eventControllers.js
@@ -1,32 +1,53 @@
-const { Event } = require('../../db/models');
+const { Event, Order, User } = require("../../db/models");
 
 exports.getEvent = async (req, res) => {
-    const event = await Event.findAll();
-    res.json(event);
+  const event = await Event.findAll();
+  res.json(event);
 };
 
 exports.getTheEvent = async (req, res) => {
-    //console.log('req.params========>', req.params)
-    const eventId = req.params.id;
-    //console.log('eventId========>', eventId)
-    const theEvent = await Event.findAll({
-        where: { id: eventId },
-        raw: true,
-    });
-    res.json(theEvent);
+  //console.log('req.params========>', req.params)
+  const eventId = req.params.id;
+  //console.log('eventId========>', eventId)
+  const theEvent = await Event.findAll({
+    where: { id: eventId },
+    raw: true,
+  });
+  res.json(theEvent);
 };
 
 exports.regEvent = async (req, res) => {
-    console.log('req.body========>', req.body)
-    const { ticketQT } = req.body
-    console.log('ticketQT========>', ticketQT)
-    const eventId = req.params.id;
-    console.log('eventId========>', eventId)
-    const toEvent = await Event.findAll({
-        where: { id: eventId },
-        raw: true,
-    });
-    console.log('toEvent========>', toEvent)
+  //console.log('req.body========>', req.body)
+  const { ticketQT } = req.body;
+  //console.log('ticketQT========>', ticketQT)
+  const eventId = req.params.id;
+  //console.log('eventId========>', eventId)
+  const toEvent = await Event.findOne({
+    where: { id: eventId },
+    raw: true,
+  });
+  const totalPrice = ticketQT * toEvent.price;
+  /*   console.log("totalprice", totalPrice);
+  console.log("toEvent========>", toEvent); */
+  const order = await Order.create({
+    ticketQT,
+    totalprice: totalPrice,
+    UserId: 2,
+    EventId: eventId,
+  });
 
-    //res.json(ticketQT);
+  const ticketLeft = toEvent.ticket - ticketQT;
+  console.log("ticketLeft========>", ticketLeft);
+  const sellTicket = await Event.update(
+    { ticket: ticketLeft },
+    { where: { id: eventId } }
+  );
+
+  const listEventUser = await Event.findAll({
+    where: { id: 2 },
+    include: [{ model: User, trough: Order }],
+    raw: true,
+  });
+  console.log("listEventUser========>", listEventUser);
+  res.json(listEventUser);
 };
