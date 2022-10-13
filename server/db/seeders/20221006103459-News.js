102c15b057dcfb3a9db782c960d2835f62c343f8
Ruslan Gamzaev <rrrruuus@gmail.com>
2022-10-13 20:46:24 +0300

    'fix2'

--- server/db/seeders/20221006103459-News.js
+++ server/db/seeders/20221006103459-News.js
@@ -13,7 +13,7 @@ module.exports = {
           pic: "https://bitok.blog/uploads/images/80E6xGuRD0vftC1ZcvoAO-Bj4P0eMy5M.jpg",
 
           createdAt: new Date(),
-          updatedAt: new Date(),git 
+          updatedAt: new Date(),
         },
         {
           id: "2",
